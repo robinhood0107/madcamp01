d3d330fcb95fae049f7804b4fe13ef92d6f27a68
jongjm1023 <jongjm1023@gmail.com>
2026-01-09 19:52:48 +0900

    firebase 연동
    갤러리에서 사진 가져와서 제목과 함께 firebase 업로드 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,10 +40,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
     implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-firestore")
+    implementation ("com.google.firebase:firebase-storage")
 }
