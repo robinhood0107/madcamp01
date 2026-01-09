9ee94d9ccd3724214838090fc389823e1377ab78
jongjm1023 <jongjm1023@gmail.com>
2026-01-09 13:13:56 +0900

    오타 수정
    wirte->write

--- app/src/main/java/com/example/madcamp01/WriteFragment.java
+++ app/src/main/java/com/example/madcamp01/WriteFragment.java
@@ -12,6 +12,7 @@ public class WriteFragment extends Fragment { // 클래스 이름
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // 이 프래그먼트와 연결된 화면(XML)을 불러오는 코드
-        return inflater.inflate(R.layout.fragment_wirte, container, false);
+
+        return inflater.inflate(R.layout.fragment_write, container, false);
     }
 }
