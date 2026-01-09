ba6da038d36ec024c21e1bd90fe4e68d393c9aa0
robinhood0107_LAPTOP <pjjpjj111@naver.com>
2026-01-09 22:42:24 +0900

    feat: 세부 이미지 갤러리(bg_rounded_footer.xml, DetailAdapter, DetailFragment, fragment_detail.xml, item_detail_footer.xml, item_detail_image.xml) 및 개인 사진 게시판(fragment_list.xml, item_post.xml, ListFragment.java, PostAdapter, PostItem) 작성 완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.appcheck.debug)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
@@ -49,4 +51,5 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation ("com.google.firebase:firebase-firestore")
     implementation ("com.google.firebase:firebase-storage")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
